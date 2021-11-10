2c9edbd3512021180a83476e658ade0ca5770942
hassan <hassanhafez86@gmail.com>
2021-11-10 15:59:45 +0000

    added types for index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,36 @@
 import type { NextPage } from 'next'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
 
-const Home: NextPage = () => {
+type IResponse = {
+  json(): Promise<any>
+  ok?: boolean | undefined
+  statusCode?: number | undefined
+}
+
+type IProperties = {
+  id: number
+  address: string
+  postcode: string
+  propertyType: string
+}[]
+
+export const getStaticProps: GetStaticProps = async (context) => {
+
+  const res: IResponse = await fetch(`http://localhost:3000/api/hello`)
+  const data: IProperties[] = await res.json()
+  const errorCode = res.ok ? 200 : res.statusCode
+
+  return {
+    props: {
+      data,
+      errorCode
+    }
+  }
+}
+
+const Home: NextPage = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  
+  console.log('data', data)
   
   return (
     <div>
